123a9154b15e5bbcab6210d8ed00746be2e2ccfe
Matheus Castello <matheus.castello@toradex.com>
2024-01-27 22:14:26 -0300

    monoDemo: Utils: Add TorizonInfo
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

---
+++ monoDemo/Hardware/DemoApalisIMX8.cs
@@ -0,0 +1,19 @@
+
+using System.Device.Gpio;
+using Iot.Device.Button;
+
+namespace Torizon
+{
+
+    public class DemoApalisIMX8 : DemoHardware
+    {
+        override protected int Gpiochip { get => 2; }
+        override protected int PinLedRed { get => 12; }
+        override protected int PinButtonRed { get => 6; }
+
+        public DemoApalisIMX8() : base()
+        {
+        }
+    }
+
+}
