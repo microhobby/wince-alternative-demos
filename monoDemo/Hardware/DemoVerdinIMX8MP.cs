f9a83d1b19ffa0a006e1974fe07b3bdff42945ed
Matheus Castello <matheus.castello@toradex.com>
2024-02-02 17:18:25 -0300

    DemoVerdinIMX8MP: Fix GPIO pin numbers
    Also comment it to document the pin numbers and the connector where
    they are located.
    
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- monoDemo/Hardware/DemoVerdinIMX8MP.cs
+++ monoDemo/Hardware/DemoVerdinIMX8MP.cs
@@ -8,8 +8,12 @@ namespace Torizon
     public class DemoVerdinIMX8MP : DemoHardware
     {
         override protected int Gpiochip { get => 0; }
-        override protected int PinLedRed { get => 1; }
-        override protected int PinButtonRed { get => 6; }
+        // SODIMM 212
+        // Yavia J3 - 24
+        override protected int PinLedRed { get => 6; }
+        // SODIMM 208
+        // Yavia J3 - 22
+        override protected int PinButtonRed { get => 1; }
 
         public DemoVerdinIMX8MP() : base()
         {
